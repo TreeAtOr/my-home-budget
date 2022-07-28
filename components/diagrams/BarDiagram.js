9662e99c50222962bbc1d7dab98377d66777a27e
TreeAtOr <gdorevino@gmail.com>
2022-07-28 20:52:59 +0300

    fix: diagrames rerendering performance

--- components/diagrams/BarDiagram.js
+++ components/diagrams/BarDiagram.js
@@ -1,16 +1,16 @@
-import { Container, Card, useTheme, Text, Row } from '@nextui-org/react';
+import { Container, Card, useTheme, Text, Row, Col } from '@nextui-org/react';
 import { Bar } from 'react-chartjs-2';
 
 export default function BarDiagram({ data, title }) {
     return (
-        <Card  css={{ mw: "925px" }}>
+        <Card css={{ mw: "925px" }}>
             <Card.Header>
                 <Row justify='center'>
-                <Text size={16} b color='grey'>{title}</Text>
+                    <Text size={16} b color='grey'>{title}</Text>
                 </Row>
             </Card.Header>
             <Card.Body>
-                {data?<Bar data={data}/>: <Container fluid alignContent='center' justify='center'><Text size={18}>You have nothing to show, yet</Text></Container>}
+                <Bar data={data?data:{ labels: [], datasets:[]}} />
             </Card.Body>
         </Card>)
 };
--- components/diagrams/DonutDiagram.js
+++ components/diagrams/DonutDiagram.js
@@ -4,14 +4,14 @@ import { Doughnut } from 'react-chartjs-2';
 export default function DonutDiagram({ data, title }) {
     console.log(data);
     return (
-        <Card  css={{ mw: "429px" }}>
+        <Card css={{ mw: "429px" }}>
             <Card.Header>
                 <Row justify='center'>
-                <Text size={16} b color='grey'>{title}</Text>
+                    <Text size={16} b color='grey'>{title}</Text>
                 </Row>
             </Card.Header>
             <Card.Body>
-                {data?<Doughnut data={data}/>: <Container fluid alignContent='center' justify='center'><Text size={18}>You have nothing to show, yet</Text></Container>}
+                <Doughnut data={data?data:{labels: [], datasets:[]}} />
             </Card.Body>
         </Card>)
 };
