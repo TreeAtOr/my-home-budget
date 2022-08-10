36aecf132b667a1d73cca54e132360d7d05ec822
TreeAtOr <gdorevino@gmail.com>
2022-08-10 16:58:31 +0300

    fix: broken config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  webpack: {
-    ec
-  }
+  swcMinify: true
 }
 
 module.exports = nextConfig
