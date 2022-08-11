df7bbab2364a747f029026f714a5be1f61cde640
TreeAtOr <gdorevino@gmail.com>
2022-08-11 20:31:03 +0300

    fix: mobile email input display

--- components/Auth.js
+++ components/Auth.js
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import { ErrorModal } from './modals/ErrorModal';
-import { Container, Text, Input, Button, Spacer, Row, Col } from "@nextui-org/react";
+import { Container, Text, Input, Button, Spacer, Row, Col, Grid } from "@nextui-org/react";
 import useTranslation from 'next-translate/useTranslation';
 import { MagicLinkModal } from './modals/MagicLinkModal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import useAdaptivity from '../utils/hooks/useAdaptivity';
 
 export default function Auth() {
   const [loading, setLoading] = useState(false)
@@ -45,6 +46,8 @@ export default function Auth() {
   const alertCloseHandler = () => setAlertVisible(false);
   const errorCloseHandler = () => setErrorVisible(false)
 
+  const size = useAdaptivity()
+
   return (
     <Container alignItems="center" css={{ height: '80vh', display: 'flex', justifyContent: 'center' }}>
       <Container>
@@ -58,26 +61,32 @@ export default function Auth() {
           <Text size={16}>{t('SignMagicLink')}</Text>
         </Row>
         <Spacer y={2} />
-        <Row justify="center" >
-          <Input
-            size="md"
-            type="email"
-            placeholder={t('placeholderEmail')}
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Spacer x={1} />
-          <Button
-            onClick={(e) => {
-              e.preventDefault()
-              handleLogin(email)
-            }}
-            size="md"
-            disabled={loading}
-          >
-            <span>{loading ? t('Loading') : t('SendMagicLink')}</span>
-          </Button>
-        </Row>
+        <Grid.Container justify="center" >
+          <Grid sm={5} xs={12} justify={size === 'xs' ? "center" : "flex-end"}  >
+            <Input
+              css={size === 'xs' ? { width: "100%" } : undefined}
+              size="md"
+              type="email"
+              placeholder={t('placeholderEmail')}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Grid>
+            <Spacer x={2} />
+          <Grid sm={5} xs={12} justify={size === 'xs' ? "center" : "flex-start"} >
+            <Button
+              css={size === 'xs' ? { width: "100%" } : undefined}
+              onClick={(e) => {
+                e.preventDefault()
+                handleLogin(email)
+              }}
+              size="md"
+              disabled={loading}
+            >
+              <span>{loading ? t('Loading') : t('SendMagicLink')}</span>
+            </Button>
+          </Grid>
+        </Grid.Container>
         <Spacer y={0.5} />
         <Row justify="center"><Text size={18} color="gray">OR</Text></Row>
         <Spacer y={0.5} />
